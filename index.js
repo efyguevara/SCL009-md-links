7c81c52b2462e3e386dce50436a5009d46efec0d
Stefany Guevara <efyguevara@gmail.com>
2019-06-25 18:25:25 -0400

    Lee si es un archivo o un directorio - Si es File ejecuta links(que contiene readFile) - Si es un directorio se ejecuta readDirectoryTerminal para leer los md que hay dentro del directorio.
    Muestra el status de cada link

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
-#!/usr/bin / env node
+#!/usr/bin/env node
+// module.exports = () => {
+// };
 
-module.exports = () => {
-
-};
-
-
+//const mdLinks = require("./md-links.js")
 const process = require('process');
+let arrFiles = []; //terminalValue
 const marked = require("marked");
-const FileHound = require('filehound');
 const fs = require('fs');
+const FileHound = require('filehound');
 const path = require('path')
 const fetch = require('node-fetch');
 
 process.argv.forEach((val, index) => {
+  arrFiles.push(process.argv[index]);
+});
+console.log("1", process.argv);
+console.log("2", arrFiles[2]);
+
+
+//Determina si es un file o un directory
+fs.stat(arrFiles[2], (error, stats) => {
+  if (error) {
+    console.log(error);
+  }
+  if (stats.isFile()) {
+    console.log("ES UN FILE");
+    links(arrFiles[2]);
+  }
+  if (stats.isDirectory()) {
+    console.log("ES UN DIRECTORIO");
+    readDirectoryTerminal(arrFiles[2]);
+  }
 });
 
+
 const links = (path) => {
-  console.log("hola")
+  console.log("READ FILE TERMINAL")
   fs.readFile(path, "utf8", (err, data) => {
     if (err) {
       throw err;
@@ -24,50 +43,41 @@ const links = (path) => {
     let links = [];
 
     const renderer = new marked.Renderer();
-
-    renderer.link = function (href, title, text) {
+    renderer.link = (href, title, text) => {
 
       links.push({
-
         href: href,
         text: text,
         file: path
       })
     }
     marked(data, { renderer: renderer })
-    // console.log(links);
-    
+
+    //Para mostrar los links
+    console.log("links:", links);
+    //Para mostrar los links ok
+    fetchLink(links);
   })
 }
-console.log("2", links("./prueba2.md"));
 
+const fetchLink = (links) => {
+  links.forEach(element => {
+    fetch(element.href)
+      .then(resolve => {
+        console.log(resolve.url);
+        console.log(resolve.ok);
+        console.log(resolve.status);
+        console.log(resolve.statusText);
+      })
+  })
+}
 
-
-
-
-
-const files = FileHound.create()
-  .discard('node_modules')
-  .paths('/home/laboratoriad160/Escritorio')
-  .ext('md')
-  .find()
-//  .then(files => {
-//   files.forEach(files => console.log('leyendo archivos', files));
-// })
-files.then((loquesea) => console.log(loquesea));
-
-
-
-// let baseUrl = "https://docs.npmjs.com/cli/link";
-// fetch(`${baseUrl}api/user/login`, {
-//   withCredentials: true,
-//   headers: myHeaders
-// })
-//   .then(function (response) {
-//     console.log(response.status); // Will show you the status
-//     if (!response.ok) {
-//       throw new Error("HTTP status " + response.status);
-//     }
-//     return response.json();
-//   })
-//   .catch(err)
+const readDirectoryTerminal = (directorio) => {
+  //Muestra los archivos que esten dentro del directorio que se pasa en "path"
+  const files = FileHound.create()
+    .discard('node_modules')
+    .paths(directorio)
+    .ext('md')
+    .find()
+  files.then(console.log);
+}
