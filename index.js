3a87a4450f0110004faf45abad779d4ab70bb650
Stefany Guevara <efyguevara@gmail.com>
2019-06-25 10:50:36 -0400

    probando pushear a la nueva rama

--- index.js
+++ index.js
@@ -10,15 +10,13 @@ const marked = require("marked");
 const FileHound = require('filehound');
 const fs = require('fs');
 const path = require('path')
+const fetch = require('node-fetch');
 
-// fs.readFile("./prueba.md", "utf8",(err, data) => {
-//   if(err){
-//     throw err
-//   }
-//     console.log(data);
-// })
+process.argv.forEach((val, index) => {
+});
 
 const links = (path) => {
+  console.log("hola")
   fs.readFile(path, "utf8", (err, data) => {
     if (err) {
       throw err;
@@ -37,19 +35,15 @@ const links = (path) => {
       })
     }
     marked(data, { renderer: renderer })
-    console.log(links);
+    // console.log(links);
     
   })
 }
+console.log("2", links("./prueba2.md"));
 
-console.log("1", links("./prueba.md"));
 
-// console.log("2", links("./prueba2.md"));
 
-console.log("3", process.argv);
 
-process.argv.forEach((val, index) => {
-});
 
 
 const files = FileHound.create()
@@ -60,121 +54,20 @@ const files = FileHound.create()
 //  .then(files => {
 //   files.forEach(files => console.log('leyendo archivos', files));
 // })
-files.then((loquesea) => console.log(loquesea[6]));
-
-
-fetch(`${baseUrl}api/user/login`, {
-  withCredentials: true,
-  headers: myHeaders
-})
-  .then(function (response) {
-    console.log(response.status); // Will show you the status
-    if (!response.ok) {
-      throw new Error("HTTP status " + response.status);
-    }
-    return response.json();
-  })
-  .then();
+files.then((loquesea) => console.log(loquesea));
 
 
 
-
-// const files = FileHound.create()
-//   .paths('/home/laboratoriad160/Escritorio/SCL009-md-links/')
-//   .ext('json')
-//   .find()
-
-// .then(files => {
-//   files.forEach(file => console.log('leyendo archivos', file));
+// let baseUrl = "https://docs.npmjs.com/cli/link";
+// fetch(`${baseUrl}api/user/login`, {
+//   withCredentials: true,
+//   headers: myHeaders
 // })
-
-
-
-
-
-//CODIGO DE MARINES
-
-const path = require('path');
-const process = require("process");
-const marked = require("marked");
-
-const fs = require('fs');
-
-const FileHound = require('filehound');
-
-const files = FileHound.create()
-  .paths('\\Users\\Maximiliano\\Desktop\\SCL009-md-links')
-  .ext('md')
-  .find();
-
-files.then(console.log);
-
-// fs.readFile("./prueba.md", "utf8",(err, data) => {
-//   if(err){
-//     throw err
-//   }
-
-//     console.log(data);
-
-// })
-
-
-// const links = (path) =>{
-//   fs.readFile(path,"utf8", (err,data) =>{
-//     if(err){
-//       throw err;
+//   .then(function (response) {
+//     console.log(response.status); // Will show you the status
+//     if (!response.ok) {
+//       throw new Error("HTTP status " + response.status);
 //     }
-//       let links =[];
-
-//     const renderer = new marked.Renderer();
-
-//     renderer.link = function(href, title, text){
-
-//       links.push({
-
-//         href:href,
-//         text:text,
-//         file:path
-
-//       })
-
-//     }
-//     marked(data, {renderer:renderer})
-//       console.log(links);
+//     return response.json();
 //   })
-
-// }
-
-// //console.log(links("./prueba.md"));
-// console.log(process.argv);
-// process.argv.forEach((val, index) => {
-//   console.log(`${index}: ${val}`);
-// });
-
-// const FizzBuzz = (number) => {
-
-//   if(numbre % 5 === 0 && number % 3 === 0){
-// console.log("Fizz");
-//   }
-
-//   else if(numbre%3 === 0){
-//     console.log("Buzz");
-//       }
-
-// }
-
-const grab = (flag) => {
-
-  let index = process.argv.indexOf(flag);
-  return (index === -1) ? null : process.argv[index + 1];
-
-}
-
-let greeting = grab(" ");
-let user = grab(" ");
-
-if (!user || !greeting) {
-  console.log("Marico el que lo lea");
-} else {
-  console.log(`${user} ${greeting}`);
-}
+//   .catch(err)
