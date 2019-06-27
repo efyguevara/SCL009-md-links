dfad60afca44890d859274e64ba40f1984498b32
Stefany Guevara <efyguevara@gmail.com>
2019-06-27 18:19:42 -0400

    Varios *Se declara let links = [] de forma global Cambio nombre de constante links a readLinks para pode rdeclarar ler links global Add promise a readLinks
    *Se agrega el readFile al fileHound para que lea el contenido de los links dentro de los archivos encontrados en el directorio
    
    *****Falta hacer que el readLinks imprima de forma correcta el arreglo de los links
    -si le paso como parametro el "link" en el llamado de la funcion no se muestra el arreglo
    
    -si le paso arrFiles[2] + el console.log de "res" me imprime el arreglo pero me repite el readFile

--- index.js
+++ index.js
@@ -8,15 +8,16 @@ let arrFiles = []; //terminalValue
 const marked = require("marked");
 const fs = require('fs');
 const FileHound = require('filehound');
-const path = require('path')
+const path = require('path');
 const fetch = require('node-fetch');
+
+let links = [];
 
 process.argv.forEach((val, index) => {
   arrFiles.push(process.argv[index]);
 });
-console.log("1", process.argv);
-console.log("2", arrFiles[2]);
 
+console.log("procces.argv", arrFiles[2]);
 
 //Determina si es un file o un directory
 fs.stat(arrFiles[2], (error, stats) => {
@@ -25,7 +26,7 @@ fs.stat(arrFiles[2], (error, stats) => {
   }
   if (stats.isFile()) {
     console.log("ES UN FILE");
-    links(arrFiles[2]);
+    readLinks(arrFiles[2]);
   }
   if (stats.isDirectory()) {
     console.log("ES UN DIRECTORIO");
@@ -33,41 +34,51 @@ fs.stat(arrFiles[2], (error, stats) => {
   }
 });
 
+const readLinks = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data)
 
-const links = (path) => {
-  console.log("READ FILE TERMINAL")
-  fs.readFile(path, "utf8", (err, data) => {
-    if (err) {
-      throw err;
-    }
-    let links = [];
+        const renderer = new marked.Renderer();
+        renderer.link = (href, title, text) => {
+          links.push({
+            href: href,
+            text: text,
+            file: path
+          })
+        }
+        marked(data, { renderer: renderer })
 
-    const renderer = new marked.Renderer();
-    renderer.link = (href, title, text) => {
-
-      links.push({
-        href: href,
-        text: text,
-        file: path
-      })
-    }
-    marked(data, { renderer: renderer })
-
-    //Para mostrar los links
-    console.log("links:", links);
-    //Para mostrar los links ok
-    fetchLink(links);
+        fetchLink(links);
+      }
+    })
   })
 }
 
-const fetchLink = (links) => {
-  links.forEach(element => {
+readLinks(arrFiles[2])
+  .then(res => {
+    //
+    console.log("Probando readLinks", res);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+
+const fetchLink = (readLinks) => {
+  readLinks.forEach(element => {
     fetch(element.href)
-      .then(resolve => {
-        console.log(resolve.url);
-        console.log(resolve.ok);
-        console.log(resolve.status);
-        console.log(resolve.statusText);
+      .then(res => {
+        console.log("* Url:", res.url);
+        console.log(" - Fail or true:", res.ok);
+        console.log(" - Cod:", res.status);
+        console.log(" - Status:", res.statusText);
+      })
+      .catch(err => {
+        console.log(err)
       })
   })
 }
@@ -79,5 +90,15 @@ const readDirectoryTerminal = (directorio) => {
     .paths(directorio)
     .ext('md')
     .find()
-  files.then(console.log);
+  // files.then(console.log)
+  files.then(res => {
+    res.forEach(element => {
+      console.log("Archivo:", element);
+      links(element);
+    });
+  })
+    .catch(err => {
+      console.log(err)
+    })
+
 }
--- prueba.md
+++ prueba.md
@@ -1,18 +1,5 @@
-Hola mundo
-<!-- 
-[Acerca de Node.js - Documentación oficial]()
-[Node.js file system - Documentación oficial]()
-[Node.js http.get - Documentación oficial]()
-[Node.js - Wikipedia]()
-[What exactly is Node.js? - freeCodeCamp]()
-[¿Qué es Node.js y para qué sirve? - drauta.com]()
-[¿Qué es Nodejs? Javascript en el Servidor - Fazt en YouTube]()
-[¿Simplemente qué es Node.js? - IBM Developer Works, 2011]()
-[Node.js y npm]()
-[Módulos, librerías, paquetes, frameworks... ¿cuál es la diferencia?]()
-[Asíncronía en js]()
-[NPM]() 
-[Publicar packpage]() -->
+****Este es el archivo de prueba**** 
+
 
 [Crear módulos en Node.js](https://docs.npmjs.com/packages-and-modules/contributing-packages-to-the-registry)
 
