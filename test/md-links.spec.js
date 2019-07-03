1ddd16242b0352453cd721f730302aadc4340dba
Stefany Guevara <efyguevara@gmail.com>
2019-07-03 18:11:45 -0400

    Agrego archivo error.js, test de archivo .md sin options

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,9 +1,17 @@
 const mdLinks = require('../md-links.js');
 
-test('Resuelve un objeto con las estadisticas de los links analizados en el archivo: Tota de links, links unicos y links validos', async () => {
-  await expect
-  return mdLinks('./prueba.md', { validate: true, stats: true }).then(data => {
-    expect(data).toEqual({ "Broken": 1, "Total": 8, "Unique": 6 });
+test('Resuelve un objeto con la información de cada link', () => {
+  expect.assertions(1);
+  return mdLinks('src/prueba2.md').then(data => {
+    expect(data).toEqual([ { href: 'https://nodejs.org/api/fs.html#fs_fs_readdir_path_options_callback',
+    text: 'Leer un directorio',
+    file: 'src/prueba2.md' },
+  { href: 'https://nodejs.org/api/fs.html#fs_fs_readdir_path_options_callback',
+    text: 'Leer un directorio',
+    file: 'src/prueba2.md' },
+  { href: 'https://github.com/efyg/SCL009-md-links/blob/master/prueba.md',
+    text: 'Leer un archivo',
+    file: 'src/prueba2.md' } ]);
   });
 });
 
@@ -39,6 +47,12 @@ test(`Este comando entrega un array de objetos que contienen el href, Text, File
   );
 });
 
+test('Resuelve un objeto con las estadisticas de los links analizados en el archivo: Total de links, links unicos y links validos', async () => {
+  await expect
+  return mdLinks('./prueba.md', { validate: true, stats: true }).then(data => {
+    expect(data).toEqual({ "Broken": 1, "Total": 8, "Unique": 6 });
+  });
+});
 
 test(`Entrega un array de objetos que contienen el href, Text y File  de cada link  encontrado`, async () => {
   expect.assertions(1);
@@ -96,10 +110,14 @@ test(`Entrega un objeto con la camtidad de links que posee el directorio y cuant
 });
 
 
-test(`Entrega un objeto con la camtidad de links que posee el directorio y cuantos de ellos son unicos`, () => {
+test(`Entrega un objeto con la camtidad de links que posee el directorio y cuantos de ellos son unicos y cuantos estan rotos`, () => {
   expect.assertions(1);
   return mdLinks('./src', { validate: true, stats: true }).then(data => {
     expect(data).toEqual({ Total: 3, Unique: 2, Broken: 1 });
   });
 });
+
+
+//que sea una extensión md y que no lea un archivo con extensión md
+// si un archivo md no contiene links debe retornar {}
 
